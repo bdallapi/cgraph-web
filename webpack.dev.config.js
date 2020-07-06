805460be03bb95908464e334053cdfa728158c34
Bastien Dalla Piazza <bastien.dallapiazza@gmail.com>
2020-07-06 20:36:36 +0200

    organize further build toolchain

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: './src/main.js',
@@ -7,4 +9,15 @@ module.exports = {
         filename: 'main.js'
     },
     devtool: 'inline-source-map',
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets'}
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.ejs',
+            hash: true
+        })
+    ]
 };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: "production",
@@ -26,4 +28,15 @@ module.exports = {
             }
         })]
     },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets'}
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.ejs',
+            hash: true
+        })
+    ]
 };
