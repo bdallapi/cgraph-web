6da0ea4d1edfb2f641870896e3cf734e5d6cb4fe
Bastien Dalla Piazza <bastien.dallapiazza@gmail.com>
2020-07-29 21:56:06 +0200

    let tuneGrid be resizable

--- src/views/TuneGrid.js
+++ src/views/TuneGrid.js
@@ -16,10 +16,11 @@ class TuneGrid extends Scrollbox {
         });
         this.octaves = 3;
         this.chordsCount = 16;
-        this.gridHeight = rect.height / 12 / this.octaves;
-        this.gridWidth = rect.width / this.chordsCount;
         this.graph = new PIXI.Graphics();
         this.content.addChild(this.graph);
+        const scaleY = rect.height / 12 / this.octave;
+        const scaleX = rect.width / this.chordsCount;
+        this.content.scale.set(scaleX, scaleY);
         this.draw();
     }
     resize(rect) {
@@ -28,40 +29,41 @@ class TuneGrid extends Scrollbox {
             bowHeight: rect.height
         });
         this.position.set(rect.x, rect.y);
-        this.gridHeight = rect.height / 12 * this.octaves;
-        this.gridWidth = rect.width / this.chordsCount;
+        const scaleY = rect.height / 12 / this.octaves;
+        const scaleX = rect.width / this.chordsCount;
+        this.content.scale.set(scaleX, scaleY);
         this.update();
     }
     draw() {
         this.graph.clear();
-        this.graph.lineStyle(2, foreFrontColor, 1);
+        this.graph.lineStyle(1 / 16, foreFrontColor, 1);
         for (let o = 0; o < 3; ++o) {
-            this.graph.drawRect(0, o * 12 * this.gridHeight, this.gridWidth * this.chordsCount, this.gridHeight)
+            this.graph.drawRect(0, o * 12, this.chordsCount, 1)
                 .beginFill(foreFrontColor, 0.5)
-                .drawRect(0, (12 * o + 1) * this.gridHeight, this.gridWidth * this.chordsCount, this.gridHeight)
+                .drawRect(0, (12 * o + 1), this.chordsCount, 1)
                 .endFill()
-                .drawRect(0, (o * 12 + 2) * this.gridHeight, this.gridWidth * this.chordsCount, this.gridHeight)
+                .drawRect(0, (o * 12 + 2), this.chordsCount, 1)
                 .beginFill(foreFrontColor, 0.5)
-                .drawRect(0, (12 * o + 3) * this.gridHeight, this.gridWidth * this.chordsCount, this.gridHeight)
+                .drawRect(0, (12 * o + 3), this.chordsCount, 1)
                 .endFill()
-                .drawRect(0, (o * 12 + 4) * this.gridHeight, this.gridWidth * this.chordsCount, this.gridHeight)
-                .drawRect(0, (o * 12 + 5) * this.gridHeight, this.gridWidth * this.chordsCount, this.gridHeight)
+                .drawRect(0, (o * 12 + 4), this.chordsCount, 1)
+                .drawRect(0, (o * 12 + 5), this.chordsCount, 1)
                 .beginFill(foreFrontColor, 0.5)
-                .drawRect(0, (12 * o + 6) * this.gridHeight, this.gridWidth * this.chordsCount, this.gridHeight)
+                .drawRect(0, (12 * o + 6), this.chordsCount, 1)
                 .endFill()
-                .drawRect(0, (o * 12 + 7) * this.gridHeight, this.gridWidth * this.chordsCount, this.gridHeight)
+                .drawRect(0, (o * 12 + 7), this.chordsCount, 1)
                 .beginFill(foreFrontColor, 0.5)
-                .drawRect(0, (12 * o + 8) * this.gridHeight, this.gridWidth * this.chordsCount, this.gridHeight)
+                .drawRect(0, (12 * o + 8), this.chordsCount, 1)
                 .endFill()
-                .drawRect(0, (o * 12 + 9) * this.gridHeight, this.gridWidth * this.chordsCount, this.gridHeight)
+                .drawRect(0, (o * 12 + 9), this.chordsCount, 1)
                 .beginFill(foreFrontColor, 0.5)
-                .drawRect(0, (12 * o + 10) * this.gridHeight, this.gridWidth * this.chordsCount, this.gridHeight)
+                .drawRect(0, (12 * o + 10), this.chordsCount, 1)
                 .endFill()
-                .drawRect(0, (o * 12 + 11) * this.gridHeight, this.gridWidth * this.chordsCount, this.gridHeight);
+                .drawRect(0, (o * 12 + 11), this.chordsCount, 1);
         }
         for (let g = 1; g < this.chordsCount; ++g) {
-            this.graph.moveTo(g * this.gridWidth, 0);
-            this.graph.lineTo(g * this.gridWidth, this.octaves * 12 * this.gridHeight);
+            this.graph.moveTo(g, 0);
+            this.graph.lineTo(g, this.octaves * 12);
         }
         this.update();
     }
